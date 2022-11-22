b8452d40a041cdb2c2386944e5515b95e5f72642
alxyok <alxyok@naiama.com>
2022-11-22 19:36:42 +0000

    change variable declaration style

--- backend.tf
+++ backend.tf
@@ -2,12 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "n3-zeus"
     key     = "n3-zeus-tfstate"
-    region  = var.aws_region
     encrypt = true
-    
-    access_key = var.aws_access_key_id
-    secret_key = var.aws_secret_access_key
-
     dynamodb_table = "n3-zeus-tfstate"
   }
 }
