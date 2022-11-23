5dadfb60d0eef1c9d36485d4a515ad775f729842
alxyok <alxyok@naiama.com>
2022-11-23 17:54:41 +0000

    format .tf files and fix destroy command

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "n3-zeus-tfstate"
-    key            = "zeus.tfstate"
-    encrypt        = true
+    bucket  = "n3-zeus-tfstate"
+    key     = "zeus.tfstate"
+    encrypt = true
 
     dynamodb_table = "n3-zeus-tfstate"
   }
