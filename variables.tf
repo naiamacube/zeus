81836d1d3581dc218ee5c9ea0f2b67d37402b6f7
alxyok <alxyok@naiama.com>
2022-11-23 07:34:15 +0000

    remove unused variables

--- variables.tf
+++ variables.tf
@@ -1,20 +1,3 @@
-variable "aws_access_key_id" {
-  description = "AWS IAM User Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS IAM User Secret Access Key"
-  type        = string
-  sensitive   = true
-}
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-  sensitive   = true
-}
-
 variable "gcp_credentials" {
   description = "GCP SA Private Key"
   type        = string
